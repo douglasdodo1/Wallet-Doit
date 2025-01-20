b95b02bcb0f3f0c6818b200ae4f845ccb66b9df3
douglasdodo1 <carvalhodouglas95@gmail.com>
2025-01-20 15:11:00 -0300

    Feat:creditWidget; Feat:creditProgressBarWidget

--- frontend/lib/views/home_page.dart
+++ frontend/lib/views/home_page.dart
@@ -9,8 +9,11 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 150, // Defina a altura do AppBar
-        title: DebitWidget(),
+        backgroundColor: Color.fromARGB(255, 250, 250, 250),
+        toolbarHeight: 150,
+        title: DebitWidget(
+          debit: 1000.00,
+        ),
       ),
       body: SizedBox.expand(
         child: Column(
@@ -22,8 +25,11 @@ class MyHomePage extends StatelessWidget {
             Expanded(
               flex: 3,
               child: Container(
-                color: const Color.fromARGB(255, 9, 255, 0),
-                child: CreditWidget(),
+                color: Color.fromARGB(255, 250, 250, 250),
+                child: CreditWidget(
+                  creditUsed: 200,
+                  credit: 500,
+                ),
               ),
             ),
             Expanded(
