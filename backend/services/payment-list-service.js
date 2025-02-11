dd39879513d4210b6de9fcc1bc69af622766fe13
douglasdodo1 <carvalhodouglas95@gmail.com>
2025-02-11 14:31:30 -0300

    feat-paymentListCRUD
    git status
    git commit -m feat-paymentListCRUD

--- backend/services/payment-list-service.js
+++ backend/services/payment-list-service.js
@@ -2,9 +2,67 @@ import PaymentListRepository from '../repositories/payment-list-repository.js';
 
 const paymentListRepository = PaymentListRepository();
 
-class PaymentListService{
-    static create = async (data) => {
-        return await paymentListRepository.createPaymentList(data);
+export const PaymentListService = () =>{
+    const createPayment = async (data,cpf) => {
+        const payment = await paymentListRepository.create(data,cpf);
+
+        if (!payment){
+            throw new Error('Failed to create Payment List');
+        }
+        return payment;
+    }
+
+    const readPayment = async (data) => {
+        const payment = await paymentListRepository.read(data);
+
+        if (!payment){
+            throw new Error('Payment not found');
+        }
+
+        return payment;
+    }
+
+    const readPayments = async (data) => {
+        const payment = await paymentListRepository.readAll(data);
+
+        if (!payment){
+            throw new Error('no payments found');
+        }
+
+        return payment;
+    }
+
+    const updatePayment = async (data, id) => {
+        const paymentChecked = await paymentListRepository.read(id);
+
+        if ( paymentChecked === null){
+            throw new Error('payment not found');
+        }
+
+        const updatedPayment = await paymentListRepository.update(data, id);
+        if (updatedPayment == null){
+            throw new Error('payment not updated');
+        }
+
+        return updatedPayment;
+    }
+
+    const deletePayment = async (data) => {
+        const paymentChecked = await paymentListRepository.read();
+
+        if ( paymentChecked === null){
+            throw new Error('payment not found');
+        }
+        const deletedPayment = await paymentListRepository.delete(data);
+
+        if (deletedPayment == null){
+            throw new Error('payment not deleted');
+        }
+
+        return deletedPayment;
+    }
+    return {
+        createPayment, readPayment, readPayments, updatePayment, deletePayment,
     }
 }
 
